199bb279d53d576512929111ec57b0338887e8ac
Aditya Kumar <adityavardhan09092@gmail.com>
2026-01-20 09:46:23 +0000

    practice: ch1 - sum, area, average

---
+++ Python/L1(PQ).py
@@ -0,0 +1,42 @@
+# Q1 Write A Program To Input 2 Num & Print Their Sum 
+
+Num1 = int(input("Enter Num1 :"));
+Num2 = int(input("Enter Num2 :"));
+
+print("Sum = " , Num1+Num2);
+
+
+# Q2 Write A Program To Input Of A Square & Print Its Area 
+
+Square = float(input("Enter Side Of An Square : "))
+print(Square**2)
+
+
+# Q3 Write A Program To Input 2 Floating Point Num & Print Their Average 
+
+val1 = float(input("Enter First Num : "))
+val2 = float(input("Enter Second Num : "))
+average = val1 + val2 / 2 
+print("Average Of Two Num : ",average)
+
+
+# Q4 Write A Program To Input 2 int Num a & b , Print True if a is greater Than or equal to b . If not Print False
+
+a = int(input("a = "))
+b = int(input("b = "))
+x = a>=b 
+print(x)
+
+
+#or 
+
+
+a = int(input("a = "))
+b = int(input("b = "))
+print(a>=b)
+
+a = 50 
+b = 20
+# True
+
+
